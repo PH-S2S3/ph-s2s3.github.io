3de1c72a4e8af5003c07b6f03f863faacaec3bbf
S1S3 <2020461@smpinhwa.edu.my>
2024-03-03 13:27:17 +0800

    role.html function adding
    - Added function to role.html

---
+++ SCRIPT/role-script.js
@@ -0,0 +1,124 @@
+function showContainer1() {
+
+    var container1 = document.getElementById('container1');
+    var container2 = document.getElementById('container2');
+    var container3 = document.getElementById('container3');
+    var container4 = document.getElementById('container4');
+    var container5 = document.getElementById('container5');
+    var container6 = document.getElementById('container6');
+    var container7 = document.getElementById('container7');
+    var container8 = document.getElementById('container8');
+    var container9 = document.getElementById('container9');
+    var container10 = document.getElementById('container10');
+    var container11 = document.getElementById('container11');
+    var container12 = document.getElementById('container12');
+    var container13 = document.getElementById('container13');
+    var container14 = document.getElementById('container14');
+    var container15 = document.getElementById('container15');
+    var container16 = document.getElementById('container16');
+    var container17 = document.getElementById('container17');
+    var container18 = document.getElementById('container18');
+    var container19 = document.getElementById('container19');
+    var container20 = document.getElementById('container20');
+    var container21 = document.getElementById('container21');
+    var container22 = document.getElementById('container22');
+    var container23 = document.getElementById('container23');
+    var container24 = document.getElementById('container24');
+    var container25 = document.getElementById('container25');
+    var container26 = document.getElementById('container26');
+    var container27 = document.getElementById('container27');
+    var container28 = document.getElementById('container28');
+    var container29 = document.getElementById('container29');
+    var container30 = document.getElementById('container30');
+    var container31 = document.getElementById('container31');
+    var container32 = document.getElementById('container32');
+    var container33 = document.getElementById('container33');
+    var container34 = document.getElementById('container34');
+    var container35 = document.getElementById('container35');
+    var container36 = document.getElementById('container36');
+    var container37 = document.getElementById('container37');
+    var container38 = document.getElementById('container38');
+    var container39 = document.getElementById('container39');
+    var closeBtn = document.getElementById('closeBtn');
+
+    //Show container
+    container1.style.visibility = 'visible';
+    container1.style.position = 'absolute'
+    container1.style.top = '0';
+    container1.style.left = '0';
+    container1.style.width = '100%';
+    container1.style.height = '100vh';
+    container1.style.objectFit = 'cover';
+    container1.style.zIndex = '2';
+
+    //Showing close button
+    closeBtn.style.visibility = 'visible';
+    closeBtn.style.color = 'black';
+}
+
+function showContainer2() {
+
+    var container1 = document.getElementById('container1');
+    var container2 = document.getElementById('container2');
+    var container3 = document.getElementById('container3');
+    var container4 = document.getElementById('container4');
+    var container5 = document.getElementById('container5');
+    var container6 = document.getElementById('container6');
+    var container7 = document.getElementById('container7');
+    var container8 = document.getElementById('container8');
+    var container9 = document.getElementById('container9');
+    var container10 = document.getElementById('container10');
+    var container11 = document.getElementById('container11');
+    var container12 = document.getElementById('container12');
+    var container13 = document.getElementById('container13');
+    var container14 = document.getElementById('container14');
+    var container15 = document.getElementById('container15');
+    var container16 = document.getElementById('container16');
+    var container17 = document.getElementById('container17');
+    var container18 = document.getElementById('container18');
+    var container19 = document.getElementById('container19');
+    var container20 = document.getElementById('container20');
+    var container21 = document.getElementById('container21');
+    var container22 = document.getElementById('container22');
+    var container23 = document.getElementById('container23');
+    var container24 = document.getElementById('container24');
+    var container25 = document.getElementById('container25');
+    var container26 = document.getElementById('container26');
+    var container27 = document.getElementById('container27');
+    var container28 = document.getElementById('container28');
+    var container29 = document.getElementById('container29');
+    var container30 = document.getElementById('container30');
+    var container31 = document.getElementById('container31');
+    var container32 = document.getElementById('container32');
+    var container33 = document.getElementById('container33');
+    var container34 = document.getElementById('container34');
+    var container35 = document.getElementById('container35');
+    var container36 = document.getElementById('container36');
+    var container37 = document.getElementById('container37');
+    var container38 = document.getElementById('container38');
+    var container39 = document.getElementById('container39');
+    var closeBtn = document.getElementById('closeBtn');
+
+    //Show container
+    container2.style.visibility = 'visible';
+    container2.style.position = 'absolute'
+    container2.style.top = '0';
+    container2.style.left = '0';
+    container2.style.width = '100%';
+    container2.style.height = '100vh';
+    container2.style.objectFit = 'cover';
+    container2.style.zIndex = '2';
+
+    //Showing close button
+    closeBtn.style.visibility = 'visible';
+    closeBtn.style.color = 'black';
+}
+
+function closeContainer() {
+    container1.style.visibility = 'hidden';
+    container2.style.visibility = 'hidden';
+    closeBtn.style.color = 'transparent';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+});
