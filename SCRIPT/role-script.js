bd19c04bf9f418f0f59ea1be1776d7842c488186
S1S3 <2020461@smpinhwa.edu.my>
2024-05-05 15:23:13 +0800

    ROLE MENU
    added role.html menu

--- SCRIPT/role-script.js
+++ SCRIPT/role-script.js
@@ -1,3 +1,178 @@
+document.addEventListener("orientationchange", function (event) {
+    switch (window.orientation) {
+        case -90: case 90:  /* Device is in landscape mode */
+            break;
+        default:        /* Device is in portrait mode */
+    }
+});
+
+var video = document.createElement('video');
+
+function home() {
+
+
+    video.src = 'VIDEO/Fire Ring Animation.mp4';
+    video.autoplay = true;
+    video.controls = false;
+    video.muted = false;
+    video.style.visibility = "visible";
+    video.style.position = 'fixed';
+    video.style.top = '0';
+    video.style.left = '0';
+    video.style.width = '100%';
+    video.style.height = '100%';
+    video.style.objectFit = 'cover';
+
+    window.addEventListener('resize', () => {
+        video.style.width = '100%';
+        video.style.height = '100%';
+    });
+
+    const box = document.getElementById('box');
+
+    box.appendChild(video);
+
+    video.onended = function () { hyperlink() }
+
+    function hyperlink() {
+        document.location.href = "index.html";
+    }
+}
+
+function photo() {
+
+    video.src = 'VIDEO/lock.mp4';
+    video.autoplay = true;
+    video.controls = false;
+    video.muted = false;
+    video.style.visibility = "visible";
+    video.style.position = 'fixed';
+    video.style.top = '0';
+    video.style.left = '0';
+    video.style.width = '100%';
+    video.style.height = '100%';
+    video.style.objectFit = 'cover';
+
+    window.addEventListener('resize', () => {
+        video.style.width = '100%';
+        video.style.height = '100%';
+    });
+
+    const box = document.getElementById('box1');
+
+    box.appendChild(video);
+
+    video.onended = function () { hyperlink() }
+
+    function hyperlink() {
+        document.location.href = "photo-password.html";
+    }
+
+}
+
+function vvideo() {
+    document.location.href = "video-password.html";
+}
+
+function peranan() {
+
+
+    video.src = 'VIDEO/fire.mp4';
+    video.autoplay = true;
+    video.controls = false;
+    video.muted = false;
+    video.style.visibility = "visible";
+    video.style.position = 'fixed';
+    video.style.top = '0';
+    video.style.left = '0';
+    video.style.width = '100%';
+    video.style.height = '100%';
+    video.style.objectFit = 'cover';
+
+    window.addEventListener('resize', () => {
+        video.style.width = '100%';
+        video.style.height = '100%';
+    });
+
+    const box = document.getElementById('box');
+
+    box.appendChild(video);
+
+    video.onended = function () { hyperlink() }
+
+    function hyperlink() {
+        document.location.href = "role.html";
+    }
+}
+
+function insta() {
+
+
+
+    video.src = 'VIDEO/fire.mp4';
+    video.autoplay = true;
+    video.controls = false;
+    video.muted = false;
+    video.style.visibility = "visible";
+    video.style.position = 'fixed';
+    video.style.top = '0';
+    video.style.left = '0';
+    video.style.width = '100%';
+    video.style.height = '100%';
+    video.style.objectFit = 'cover';
+
+    window.addEventListener('resize', () => {
+        video.style.width = '100%';
+        video.style.height = '100%';
+    });
+
+    const box = document.getElementById('box');
+
+    box.appendChild(video);
+
+    video.onended = function () { hyperlink() }
+
+    function hyperlink() {
+        window.open('https://www.instagram.com/ph_s1s3/', '_blank');
+        video.style.visibility = "hidden";
+    }
+}
+
+function discord() {
+
+
+
+    video.src = 'VIDEO/fire.mp4';
+    video.autoplay = true;
+    video.controls = false;
+    video.muted = false;
+    video.style.visibility = "visible";
+    video.style.position = 'fixed';
+    video.style.top = '0';
+    video.style.left = '0';
+    video.style.width = '100%';
+    video.style.height = '100%';
+    video.style.objectFit = 'cover';
+
+    window.addEventListener('resize', () => {
+        video.style.width = '100%';
+        video.style.height = '100%';
+    });
+
+    const box = document.getElementById('box');
+
+    box.appendChild(video);
+
+    video.onended = function () { hyperlink() }
+
+
+    function hyperlink() {
+        window.open('https://discord.gg/vN7USfE5hh', '_blank');
+        video.style.visibility = "hidden";
+    }
+
+}
+
 function showContainer1() {
 
     var container1 = document.getElementById('container1');
